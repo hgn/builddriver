55725547f32989685942ea12273d223c7c560338
Hagen Paul Pfeifer <hagen@jauu.net>
2019-06-14 20:54:55 +0200

    regex: parse even older gcc without column
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- gccoutputparser/gccoutputparser.py
+++ gccoutputparser/gccoutputparser.py
@@ -5,18 +5,31 @@ import re
 import os
 import sys
 
-RE_MAIN = re.compile('^(.*):(\\d+):(\\d+):.*?(warning|error):(.*)$')
+RE_GCC_WITH_COLUMN = re.compile('^(.*):(\\d+):(\\d+):.*?(warning|error):(.*)$')
+RE_GCC_WITHOUT_COLUMN = re.compile('^(.*):(\\d+):.*?(warning|error):(.*)$')
+
+
 
 class GccOutputParser(object):
 
     def __init__(self):
         pass
 
+    def _process_gcc_with_column(self, m):
+        print(m.group(), file=sys.stderr)
+
+    def _process_gcc_without_column(self, m):
+        print(m.group(), file=sys.stderr)
+
+
     def feed(self, lines):
         for line in lines.splitlines():
-            m = RE_MAIN.match(line)
+            m = RE_GCC_WITH_COLUMN.match(line)
             if m:
-                print(m.group(), file=sys.stderr)
+                return self._process_gcc_with_column(m)
+            m = RE_GCC_WITHOUT_COLUMN.match(line)
+            if m:
+                return self._process_gcc_without_column(m)
 
 
 
--- gccoutputparser/tests/test_gccoutputparser.py
+++ gccoutputparser/tests/test_gccoutputparser.py
@@ -10,8 +10,15 @@ class TestGccOutputParser(TestCase):
 
     def test_is_initiable(self):
         e = gccoutputparser.GccOutputParser()
+        del e
 
     def test_single_line(self):
         msg = 'queue.c:34:6: error: unused variable ‘foo’ [-Werror=unused-variable]'
+        msg = ' /usr/include/c++/4.9.2/bits/stl_algo.h:2267:error: elements in iterator range [__first, __last) are not partitioned by the value __val. '
+        e = gccoutputparser.GccOutputParser()
+        e.feed(msg)
+
+    def test_single_line_2(self):
+        msg = ' /usr/include/c++/4.9.2/bits/stl_algo.h:2267:error: elements in iterator range [__first, __last) are not partitioned by the value __val. '
         e = gccoutputparser.GccOutputParser()
         e.feed(msg)
