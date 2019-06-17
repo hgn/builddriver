546123fd0ed16be5939625b134fa11785a354a0d
Hagen Paul Pfeifer <hagen@jauu.net>
2019-06-17 21:17:15 +0200

    add data class for recording
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- gccoutputparser/gccoutputparser.py
+++ gccoutputparser/gccoutputparser.py
@@ -4,25 +4,63 @@
 import re
 import os
 import sys
+from dataclasses import dataclass
+
 
 RE_GCC_WITH_COLUMN = re.compile('^(.*):(\\d+):(\\d+):.*?(warning|error):(.*)$')
 RE_GCC_WITHOUT_COLUMN = re.compile('^(.*):(\\d+):.*?(warning|error):(.*)$')
 
+@dataclass
+class Entry:
+    '''
+    Holds information about ONE gcc warning/error in
+    an unified fashion - no matter if gcc/clang or version
+    '''
+    file: str
+    lineno: int
+    severity: str
+    column: int = None
 
 
-class GccOutputParser(object):
+
+class GccOutputParser:
 
     def __init__(self, val2="default value", **kwargs):
         self._parsed_lines = 0
+        self._db = list()
+        # optional tracing
         self._trace_unmatched = kwargs.get('trace_unmatched', False)
         self._trace_unmatched_db = list()
         self._trace_unmatched_no = 0
 
+    @staticmethod
+    def error_warning_selector(string):
+        if 'error' in string:
+            return 'error'
+        if 'warning' in string:
+            return 'warning'
+        return 'unknown'
+
+    def _process_new_entry(self, entry):
+        self._db.append(entry)
+        sys.stderr.write('\n')
+        sys.stderr.write(str(entry))
+        sys.stderr.write('\n')
+
     def _process_gcc_with_column(self, m):
-        pass
+        file_ = m.group(1)
+        lineno = m.group(2)
+        column = m.group(3)
+        severity = self.error_warning_selector(m.group(4))
+        e = Entry(file_, lineno, severity, column)
+        self._process_new_entry(e)
 
     def _process_gcc_without_column(self, m):
-        pass
+        file_ = m.group(1)
+        lineno = m.group(2)
+        severity = self.error_warning_selector(m.group(3))
+        e = Entry(file_, lineno, severity)
+        self._process_new_entry(e)
 
     def _process_trace_unmachted(self, line):
         self._trace_unmatched_no += 1
@@ -41,7 +79,7 @@ class GccOutputParser(object):
     def parsed_lines(self):
         return self._parsed_lines
 
-    def feed(self, lines):
+    def record(self, lines):
         for line in lines.splitlines():
             line = line.rstrip()
             self._parsed_lines += 1
@@ -55,6 +93,9 @@ class GccOutputParser(object):
                 continue
             # trace unmachted, if enabled
             self._process_trace_unmachted(line)
+
+    # just an alias, call what you want
+    feed = record
 
 
 
